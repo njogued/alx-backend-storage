29fce17e45e268424fe342b43aac7eda891fc1d8
njogued <njogued@gmail.com>
2023-07-18 16:22:15 -0700

    Pymongo all docs in collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """List all the databases using pymongo"""
+
+
 def list_all(mongo_collection):
     """Function that return a list of docs in collection"""
     if mongo_collection:
