06ba96301c2a006aa7f93a6fb7a204b97bab5dab
njogued <njogued@gmail.com>
2023-07-19 21:17:28 +0000

    redis cache class

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""1. Cache class with init and store method"""
+import redis
+import uuid
+
+
+class Cache:
+    """Cache class to implement redis"""
+
+    def __init__():
+        """Create and store an instance of Redis client"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data:Union[str, bytes, int, float]) -> str:
+        """Generate a random key and return it"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
