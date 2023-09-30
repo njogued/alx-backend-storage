627ac5e7a3aab64f510741769e04cb1043bee51d
njogued <njogued@gmail.com>
2023-09-30 22:56:58 +0300

    mysql function

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- SQL Function that calculates a / b
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN (0);
+    ELSE
+        RETURN (a / b);
+    END IF;
+END $$
+
+DELIMITER ;
