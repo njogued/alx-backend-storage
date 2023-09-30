4658f62214dffbd00be7559cf9ae876562f38389
njogued <njogued@gmail.com>
2023-09-30 23:23:14 +0300

    creating a view

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,9 @@
+-- CREATING A VIEW IN MySQL
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80 AND
+(last_meeting IS NULL
+OR
+last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
